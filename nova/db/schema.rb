f0e41e4422dcb5feed4586145fe0e05229004e3b
kanebest <kanetsuki.ryoma@moneyforward.co.jp>
2018-05-09 17:47:02 +0900

    balanceの追加

--- nova/db/schema.rb
+++ nova/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_08_025253) do
+ActiveRecord::Schema.define(version: 2018_05_09_062233) do
 
   create_table "charges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2018_05_08_025253) do
     t.boolean "staff", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "balance", default: 0
   end
 
   add_foreign_key "charges", "users"
