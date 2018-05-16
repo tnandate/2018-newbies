ead4f6f884d4d64d44c79e69d34816bedd50591f
kanebest <kanetsuki.ryoma@moneyforward.co.jp>
2018-05-16 14:19:46 +0900

    balanceのamountを10,000,000を許容

--- nova/app/models/balance.rb
+++ nova/app/models/balance.rb
@@ -1,6 +1,6 @@
 class Balance < ApplicationRecord
   MIN_BALANCE_AMOUNT = 0
-  MAX_BALANCE_AMOUNT = 100_000
+  MAX_BALANCE_AMOUNT = 10_000_000
   belongs_to :user
 
   validates :amount, numericality: { greater_than_or_equal_to: MIN_BALANCE_AMOUNT, less_than_or_equal_to: MAX_BALANCE_AMOUNT,only_integer: true }, presence: true
