686f9913967e4e559133351f779b4c0008eecd16
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-05-16 10:54:15 +0900

    stripeの例外処理を追加

--- nova/app/models/charge.rb
+++ nova/app/models/charge.rb
@@ -18,6 +18,28 @@ class Charge < ApplicationRecord
       currency: 'jpy',
       customer: user.stripe_id
     )
+  rescue Stripe::CardError => e
+    # Since it's a decline, Stripe::CardError will be caught
+    body = e.json_body
+    err  = body[:error]
+
+    puts "Status is: #{e.http_status}"
+    puts "Type is: #{err[:type]}"
+    puts "Charge ID is: #{err[:charge]}"
+    # The following fields are optional
+    puts "Code is: #{err[:code]}" if err[:code]
+    puts "Decline code is: #{err[:decline_code]}" if err[:decline_code]
+    puts "Param is: #{err[:param]}" if err[:param]
+    puts "Message is: #{err[:message]}" if err[:message]
+  rescue Stripe::RateLimitError => e
+    # Too many requests made to the API too quickly
+  rescue Stripe::InvalidRequestError => e
+    # Invalid parameters were supplied to Stripe's API
+  rescue Stripe::AuthenticationError => e
+    # Authentication with Stripe's API failed
+    # (maybe you changed API keys recently)
+  rescue Stripe::APIConnectionError => e
+    # Network communication with Stripe failed
   rescue Stripe::StripeError => e
     errors.add(:user, e.code.to_s.to_sym)
     throw :abort
