8f37675b11006a3c09492e69f7393a7a917e0b48
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-05-17 13:24:21 +0900

    Stripe::CardErrorの追加

--- nova/app/models/charge.rb
+++ nova/app/models/charge.rb
@@ -18,7 +18,7 @@ class Charge < ApplicationRecord
       currency: 'jpy',
       customer: user.stripe_id
     )
-  rescue Stripe::StripeError => e
+  rescue Stripe::CardError, Stripe::StripeError => e
     Rails.logger.error("code: " + e.code.to_s)
     Rails.logger.error("http_status: " + e.http_status.to_s)
     Rails.logger.error("json: " + e.json_body.to_s)
