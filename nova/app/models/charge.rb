6645d4d51ba916170fe75c3ea6cece1b7a889e0b
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-05-17 16:30:38 +0900

    loggerの削除とerrors.addの追加

--- nova/app/models/charge.rb
+++ nova/app/models/charge.rb
@@ -19,9 +19,7 @@ class Charge < ApplicationRecord
       customer: user.stripe_id
     )
   rescue Stripe::CardError, Stripe::StripeError => e
-    Rails.logger.error("code: " + e.code.to_s)
-    Rails.logger.error("http_status: " + e.http_status.to_s)
-    Rails.logger.error("json: " + e.json_body.to_s)
+    errors.add(:base, e.message)
     throw :abort
   end
 end
