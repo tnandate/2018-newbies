ef8d6782519ea3f701e5b785511c59c25d656bf8
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-05-17 14:39:29 +0900

    card_declinedのエラーケースの追加

--- nova/spec/models/charge_spec.rb
+++ nova/spec/models/charge_spec.rb
@@ -13,6 +13,20 @@ RSpec.describe Charge, type: :model do
     it { is_expected.to belong_to(:user) }
   end
 
+  context 'Error handlings' do
+    context 'Card Error' do
+      it 'mocks a declined card error' do
+        StripeMock.prepare_card_error(:card_declined)
+
+        expect { Stripe::Charge.create(amount: 1, currency: 'usd') }.to raise_error {|e|
+          expect(e).to be_a Stripe::CardError
+          expect(e.http_status).to eq(402)
+          expect(e.code).to eq('card_declined')
+        }
+      end
+    end
+  end
+
   describe 'Associations' do
     describe 'amount' do
       it { is_expected.to validate_numericality_of(:amount).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(100_000).only_integer }
