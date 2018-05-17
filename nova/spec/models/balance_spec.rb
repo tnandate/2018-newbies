599ea8460d5daa9ff0446a7903136649cfb577c2
kanebest <kanetsuki.ryoma@moneyforward.co.jp>
2018-05-17 16:39:57 +0900

    balanceのバリデーションの追加

--- nova/spec/models/balance_spec.rb
+++ nova/spec/models/balance_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Balance, type: :model do
   describe 'Association' do
     it { is_expected.to belong_to(:user) }
   end
+
+  describe 'Validations' do
+    describe 'amount' do
+      it { is_expected.to validate_numericality_of(:amount).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(100_000).only_integer }
+      it { is_expected.to validate_presence_of(:amount) }
+    end
+  end
 end
